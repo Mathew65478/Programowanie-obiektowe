d73c9e7662ab0fa2112644cdff6bd69fefdc8d66
Mathew65478 <mateuszgalecki@interia.pl>
2025-04-04 12:48:25 +0200

    Add files via upload

---
+++ Cw_1c/Cw_1c/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Cw_1c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 wyrazów ciągu Fibonacciego");
+            int suma;
+            int a = 0;
+            int b = 1;
+
+            
+            for (int i = 0; i < 10; i++) 
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("F{0} = " + i, i);
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine("F{0} = " + i, i);
+                }
+                else 
+                {
+                    suma = a + b;
+                    a = b;
+                    b = suma;
+
+                    Console.WriteLine("F{0} = " + suma, i);
+                }
+
+            }
+        }
+    }
+}
